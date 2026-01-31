392e9dd0c877295aa197889375aa89c0a496a112
mertwole <mertwole@gmail.com>
2026-01-31 14:34:19 +0000

    v3

--- app/src/wire/concat.rs
+++ app/src/wire/concat.rs
@@ -17,106 +17,60 @@ impl<WL: Wire, WR: Wire> Bus<2> for WireAndWire<WL, WR> {
     }
 }
 
-struct WireMarker;
-struct BusMarker;
+trait ConcatMarker {}
 
-trait ConcatenableMarker {}
+struct WireConcatMarker;
+impl ConcatMarker for WireConcatMarker {}
 
-impl ConcatenableMarker for WireMarker {}
-impl ConcatenableMarker for BusMarker {}
+struct BusConcatMarker;
+impl ConcatMarker for BusConcatMarker {}
 
-trait ConcatWire<WIRE: Wire, const W: usize, M: ConcatenableMarker>
-where
-    [(); W + 1]:,
+trait Concat<
+    const LEFT_BUS_WIDTH: usize,
+    const RIGHT_BUS_WIDTH: usize,
+    ML: ConcatMarker,
+    MR: ConcatMarker,
+> where
+    [(); LEFT_BUS_WIDTH + RIGHT_BUS_WIDTH]:,
 {
-    fn concat(self, wire: WIRE) -> impl Bus<{ W + 1 }>;
+    fn concat(self) -> impl Bus<{ LEFT_BUS_WIDTH + RIGHT_BUS_WIDTH }>;
 }
 
-impl<const W: usize, B: Bus<W>, WIRE: Wire> ConcatWire<WIRE, W, BusMarker> for B
-where
-    [(); W + 1]:,
-{
-    fn concat(self, wire: WIRE) -> impl Bus<{ W + 1 }> {
-        self.append_wire_right(wire)
-    }
-}
-
-impl<WIRE: Wire> ConcatWire<WIRE, 1, WireMarker> for WIRE {
-    fn concat(self, wire: WIRE) -> impl Bus<2> {
+impl<WL: Wire, WR: Wire> Concat<1, 1, WireConcatMarker, WireConcatMarker> for (WL, WR) {
+    fn concat(self) -> impl Bus<{ 1 + 1 }> {
         WireAndWire {
-            lhs: self,
-            rhs: wire,
+            lhs: self.0,
+            rhs: self.1,
         }
     }
 }
 
-trait ConcatBus<const WR: usize, BR: Bus<WR>, const W: usize, M: ConcatenableMarker>
+impl<const WL: usize, BL: Bus<WL>, const WR: usize, BR: Bus<WR>>
+    Concat<WL, WR, BusConcatMarker, BusConcatMarker> for (BL, BR)
 where
-    [(); W + WR]:,
+    [(); WL + WR]:,
 {
-    fn concat(self, bus: BR) -> impl Bus<{ W + WR }>;
-}
-
-impl<const W: usize, B: Bus<W>, const WR: usize, BR: Bus<WR>> ConcatBus<WR, BR, W, BusMarker> for B
-where
-    [(); W + WR]:,
-{
-    fn concat(self, bus: BR) -> impl Bus<{ W + WR }> {
-        self.append_bus_right(bus)
+    fn concat(self) -> impl Bus<{ WL + WR }> {
+        self.0.append_bus_right(self.1)
     }
 }
 
-trait ConcatWireAndBus<const WR: usize, BR: Bus<WR>>
-where
-    [(); WR + 1]:,
-{
-    fn concat(self, bus: BR) -> impl Bus<{ WR + 1 }>;
-}
+// impl<WIRE: Wire, const W: usize, B: Bus<W>> Concat<1, W, WireConcatMarker, BusConcatMarker>
+//     for (WIRE, B)
+// where
+//     [(); 1 + W]:,
+// {
+//     fn concat(self) -> impl Bus<{ W + 1 }> {
+//         self.1.append_wire_left(self.0)
+//     }
+// }
 
-impl<WIRE: Wire, const W: usize, B: Bus<W>> ConcatWireAndBus<W, B> for WIRE
-where
-    [(); W + 1]:,
-{
-    fn concat(self, bus: B) -> impl Bus<{ W + 1 }> {
-        bus.append_wire_left(self)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[derive(Clone, Copy)]
-    struct TestWire(WireState);
-
-    impl Wire for TestWire {
-        fn eval(&self) -> WireState {
-            self.0
-        }
-    }
-
-    #[derive(Clone, Copy)]
-    struct TestBus<const W: usize>([WireState; W]);
-
-    impl<const W: usize> Bus<W> for TestBus<W> {
-        fn eval(self) -> [WireState; W] {
-            self.0
-        }
-    }
-
-    const BUS_1_VALUES: [WireState; 3] = [WireState::Zero, WireState::Zero, WireState::Zero];
-    const BUS_2_VALUES: [WireState; 3] = [WireState::One, WireState::One, WireState::One];
-    const WIRE_1_VALUE: WireState = WireState::X;
-    const WIRE_2_VALUE: WireState = WireState::Z;
-
-    #[test]
-    fn test_concat() {
-        let bus_1 = TestBus(BUS_1_VALUES);
-        let bus_2 = TestBus(BUS_2_VALUES);
-        let wire_1 = TestWire(WIRE_1_VALUE);
-        let wire_2 = TestWire(WIRE_2_VALUE);
-
-        let bus_bus = bus_1.concat(bus_2);
-        let bus_wire = bus_1.concat(wire_1);
-    }
-}
+// impl<const W: usize, B: Bus<W>, WIRE: Wire> Concat<W, 1, BusConcatMarker, WireConcatMarker>
+//     for (B, WIRE)
+// where
+//     [(); 1 + W]:,
+// {
+//     fn concat(self) -> impl Bus<{ W + 1 }> {
+//         self.0.append_wire_right(self.1)
+//     }
+// }
