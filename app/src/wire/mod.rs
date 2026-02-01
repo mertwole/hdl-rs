d4a7cc0f122bd30af661afe8b23c1bf9f901e03f
mertwole <mertwole@gmail.com>
2026-02-01 12:21:03 +0100

    feat: flip-flops (#11)

--- app/src/wire/mod.rs
+++ app/src/wire/mod.rs
@@ -1,6 +1,11 @@
+use std::ops::Not;
+
+use autoimpl_operators::BitwiseOps;
+
 pub mod bus;
 pub mod bus_operators;
 pub mod concat;
+pub mod flip_flop;
 pub mod operators;
 
 pub trait Wire: Clone + Copy {
@@ -15,4 +20,84 @@ pub enum WireState {
     X,
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum LogicalWireState {
+    Zero,
+    One,
+}
+
+impl From<LogicalWireState> for WireState {
+    fn from(value: LogicalWireState) -> Self {
+        match value {
+            LogicalWireState::Zero => WireState::Zero,
+            LogicalWireState::One => WireState::One,
+        }
+    }
+}
+
+impl Not for LogicalWireState {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        match self {
+            Self::Zero => Self::One,
+            Self::One => Self::Zero,
+        }
+    }
+}
+
+// TODO: Move all the ops impl from `operators` here(as well as tests).
+impl WireState {
+    fn and(self, rhs: WireState) -> WireState {
+        if self == WireState::X || rhs == WireState::X {
+            return WireState::X;
+        }
+
+        match (self, rhs) {
+            (WireState::Zero, WireState::Zero) => WireState::Zero,
+            (WireState::Zero, WireState::One) => WireState::Zero,
+            (WireState::One, WireState::Zero) => WireState::Zero,
+            (WireState::One, WireState::One) => WireState::One,
+
+            (WireState::Zero, WireState::Z) => WireState::Zero,
+            (WireState::Z, WireState::Zero) => WireState::Zero,
+
+            (WireState::One, WireState::Z) => WireState::X,
+            (WireState::Z, WireState::One) => WireState::X,
+            (WireState::Z, WireState::Z) => WireState::X,
+            _ => unreachable!("Processed earlier"),
+        }
+    }
+}
+
 pub trait InputWire: Wire + Clone + Copy {}
+
+#[derive(Clone, Copy, BitwiseOps)]
+pub struct ConstZeroWire {}
+
+impl Wire for ConstZeroWire {
+    fn eval(&self) -> WireState {
+        WireState::Zero
+    }
+}
+
+impl ConstZeroWire {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+#[derive(Clone, Copy, BitwiseOps)]
+pub struct ConstOneWire {}
+
+impl Wire for ConstOneWire {
+    fn eval(&self) -> WireState {
+        WireState::One
+    }
+}
+
+impl ConstOneWire {
+    fn new() -> Self {
+        Self {}
+    }
+}
