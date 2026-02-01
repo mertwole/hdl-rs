33b7705b0ec3b42ec5e2e9fe589cd6e1cb0f64c5
mertwole <mertwole@gmail.com>
2026-02-01 12:07:23 +0000

    Add clock to flip-flops

--- app/src/wire/flip_flop.rs
+++ app/src/wire/flip_flop.rs
@@ -5,53 +5,57 @@ use crate::wire::{
 };
 
 #[derive(Clone, Copy)]
-pub struct FlipFlop<D: Wire, R: Wire, S: Wire> {
+pub struct FlipFlop<D: Wire, C: Wire, R: Wire, S: Wire> {
     current_state: WireState,
 
     data: D,
+    clock: C,
     reset: R,
     set: S,
 }
 
-impl<D: Wire, R: Wire, S: Wire> FlipFlop<D, R, S> {
-    pub fn new(data: D, reset: R, set: S) -> Self {
+impl<D: Wire, C: Wire, R: Wire, S: Wire> FlipFlop<D, C, R, S> {
+    pub fn new(data: D, clock: C, reset: R, set: S) -> Self {
         Self {
             current_state: WireState::X,
             data,
+            clock,
             reset,
             set,
         }
     }
 
-    pub fn resets_to_zero(data: D, reset_wire: R) -> FlipFlop<D, R, ConstZeroWire> {
-        FlipFlop::new(data, reset_wire, ConstZeroWire::new())
+    pub fn resets_to_zero(data: D, clock: C, reset_wire: R) -> FlipFlop<D, C, R, ConstZeroWire> {
+        FlipFlop::new(data, clock, reset_wire, ConstZeroWire::new())
     }
 
-    pub fn resets_to_one(data: D, reset_wire: S) -> FlipFlop<D, ConstZeroWire, S> {
-        FlipFlop::new(data, ConstZeroWire::new(), reset_wire)
+    pub fn resets_to_one(data: D, clock: C, reset_wire: S) -> FlipFlop<D, C, ConstZeroWire, S> {
+        FlipFlop::new(data, clock, ConstZeroWire::new(), reset_wire)
     }
 }
 
-impl<D: Wire, R: Wire, S: Wire> Wire for FlipFlop<D, R, S> {
+impl<D: Wire, C: Wire, R: Wire, S: Wire> Wire for FlipFlop<D, C, R, S> {
     fn eval(&self) -> WireState {
         self.current_state
     }
 }
 
 #[derive(Clone, Copy)]
-struct FlipFlopBus<const W: usize, D: Bus<W>, R: Bus<W>, S: Bus<W>> {
+struct FlipFlopBus<const W: usize, D: Bus<W>, C: Wire, R: Bus<W>, S: Bus<W>> {
     current_state: [WireState; W],
 
     data: D,
+    clock: C,
     reset: R,
     set: S,
 }
 
-impl<const W: usize, D: Bus<W>, R: Bus<W>, S: Bus<W>> FlipFlopBus<W, D, R, S> {
-    pub fn new(data: D, reset: R, set: S) -> Self {
+impl<const W: usize, D: Bus<W>, C: Wire, R: Bus<W>, S: Bus<W>> FlipFlopBus<W, D, C, R, S> {
+    pub fn new(data: D, clock: C, reset: R, set: S) -> Self {
         Self {
             current_state: [WireState::X; W],
             data,
+            clock,
             reset,
             set,
         }
@@ -59,9 +63,10 @@ impl<const W: usize, D: Bus<W>, R: Bus<W>, S: Bus<W>> FlipFlopBus<W, D, R, S> {
 
     pub fn resets_to_value<WIRE: Wire>(
         data: D,
+        clock: C,
         reset_wire: WIRE,
         reset_value: [LogicalWireState; W],
-    ) -> FlipFlopBus<W, D, impl Bus<W>, impl Bus<W>> {
+    ) -> FlipFlopBus<W, D, C, impl Bus<W>, impl Bus<W>> {
         let reset_fanout = FanoutBus::new(reset_wire);
 
         let set_mask = ConstBus::new(reset_value);
@@ -70,11 +75,13 @@ impl<const W: usize, D: Bus<W>, R: Bus<W>, S: Bus<W>> FlipFlopBus<W, D, R, S> {
         let reset_mask = ConstBus::new(reset_value.map(|value| !value));
         let reset_bus = reset_fanout.and(reset_mask);
 
-        FlipFlopBus::new(data, reset_bus, set_bus)
+        FlipFlopBus::new(data, clock, reset_bus, set_bus)
     }
 }
 
-impl<const W: usize, D: Bus<W>, R: Bus<W>, S: Bus<W>> Bus<W> for FlipFlopBus<W, D, R, S> {
+impl<const W: usize, D: Bus<W>, C: Wire, R: Bus<W>, S: Bus<W>> Bus<W>
+    for FlipFlopBus<W, D, C, R, S>
+{
     fn eval(self) -> [WireState; W] {
         self.current_state
     }
