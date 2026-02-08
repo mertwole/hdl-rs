0fe639f01bc74a78f333c97c1f21b7bfcea41532
mertwole <mertwole@gmail.com>
2026-02-08 10:30:12 +0000

    Adjust BusOps

--- app/src/api/bus/operations.rs
+++ app/src/api/bus/operations.rs
@@ -1,13 +1,10 @@
-use autoimpl_operators::{WireBitwiseOps, derive_bus_bitwise_ops};
+use autoimpl_operators::derive_bus_bitwise_ops;
 
 use crate::api::prelude::*;
 
 pub trait BusOps<const W: usize>: Bus<W> {
-    fn wire_at<const I: usize>(self) -> WireAt<W, Self, I>
-    where
-        [(); W - I - 1]:,
-    {
-        WireAt { bus: self }
+    fn wire_at<const I: usize>(self) -> SubBus<W, Self, I, 1> {
+        SubBus { bus: self }
     }
 
     fn sub_bus<const FROM: usize, const TO: usize>(self) -> SubBus<W, Self, FROM, { TO - FROM }>
@@ -18,14 +15,7 @@ pub trait BusOps<const W: usize>: Bus<W> {
         SubBus { bus: self }
     }
 
-    fn append_wire_left<WIRE: Wire>(self, wire: WIRE) -> BusWithWireToLeft<W, Self, WIRE> {
-        BusWithWireToLeft { bus: self, wire }
-    }
-
-    fn append_wire_right<WIRE: Wire>(self, wire: WIRE) -> BusWithWireToRight<W, Self, WIRE> {
-        BusWithWireToRight { bus: self, wire }
-    }
-
+    // TODO: Rename to append.
     fn append_bus_right<const WIDTH: usize, B: Bus<WIDTH>>(
         self,
         bus: B,
@@ -63,17 +53,6 @@ pub trait BusOps<const W: usize>: Bus<W> {
 
 impl<T, const W: usize> BusOps<W> for T where T: Bus<W> {}
 
-#[derive(Clone, Copy, WireBitwiseOps)]
-pub struct WireAt<const W: usize, B: Bus<W>, const I: usize> {
-    bus: B,
-}
-
-impl<const W: usize, B: Bus<W>, const I: usize> Wire for WireAt<W, B, I> {
-    fn eval(&self) -> WireState {
-        self.bus.eval()[I]
-    }
-}
-
 #[derive(Clone, Copy)]
 #[derive_bus_bitwise_ops(WIDTH)]
 pub struct SubBus<const W: usize, B: Bus<W>, const FROM: usize, const WIDTH: usize> {
@@ -85,38 +64,6 @@ impl<const W: usize, B: Bus<W>, const FROM: usize, const WIDTH: usize> Bus<WIDTH
 {
     fn eval(self) -> [WireState; WIDTH] {
         self.bus.eval()[FROM..FROM + WIDTH]
-            .try_into()
-            .expect("Checked to match the width")
-    }
-}
-
-#[derive(Clone, Copy)]
-#[derive_bus_bitwise_ops(W + 1)]
-pub struct BusWithWireToLeft<const W: usize, B: Bus<W>, WIRE: Wire> {
-    bus: B,
-    wire: WIRE,
-}
-
-impl<const W: usize, B: Bus<W>, WIRE: Wire> Bus<{ W + 1 }> for BusWithWireToLeft<W, B, WIRE> {
-    fn eval(self) -> [WireState; W + 1] {
-        [&[self.wire.eval()], &self.bus.eval()[..]]
-            .concat()
-            .try_into()
-            .expect("Checked to match the width")
-    }
-}
-
-#[derive(Clone, Copy)]
-#[derive_bus_bitwise_ops(W + 1)]
-pub struct BusWithWireToRight<const W: usize, B: Bus<W>, WIRE: Wire> {
-    bus: B,
-    wire: WIRE,
-}
-
-impl<const W: usize, B: Bus<W>, WIRE: Wire> Bus<{ W + 1 }> for BusWithWireToRight<W, B, WIRE> {
-    fn eval(self) -> [WireState; W + 1] {
-        [&self.bus.eval()[..], &[self.wire.eval()]]
-            .concat()
             .try_into()
             .expect("Checked to match the width")
     }
@@ -246,7 +193,7 @@ impl<const W: usize, B: Bus<W>, const S: usize> Bus<W> for BusShiftLeft<W, B, S>
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::api::{bus::mock::*, wire::mock::*};
+    use crate::api::bus::mock::*;
 
     const VALUES: [WireState; 8] = [
         WireState::Zero,
@@ -277,30 +224,6 @@ mod tests {
     }
 
     #[test]
-    fn test_append_wire_right() {
-        let bus = MockBus::new(VALUES);
-        let wire = MockWire::new(WireState::X);
-        let appended = bus.append_wire_right(wire);
-
-        assert_eq!(
-            appended.eval().to_vec(),
-            [&VALUES[..], &[WireState::X]].concat()
-        );
-    }
-
-    #[test]
-    fn test_append_wire_left() {
-        let bus = MockBus::new(VALUES);
-        let wire = MockWire::new(WireState::X);
-        let appended = bus.append_wire_left(wire);
-
-        assert_eq!(
-            appended.eval().to_vec(),
-            [&[WireState::X], &VALUES[..]].concat()
-        );
-    }
-
-    #[test]
     fn test_append_bus_right() {
         let bus_2_values = [WireState::X, WireState::X, WireState::X];
 
@@ -319,12 +242,12 @@ mod tests {
         let bus = MockBus::new(VALUES);
 
         let wire_4 = bus.wire_at::<4>();
-        assert_eq!(wire_4.eval(), WireState::Zero);
+        assert_eq!(wire_4.eval(), [WireState::Zero]);
 
         let wire_0 = bus.wire_at::<0>();
-        assert_eq!(wire_0.eval(), WireState::Zero);
+        assert_eq!(wire_0.eval(), [WireState::Zero]);
 
         let wire_7 = bus.wire_at::<7>();
-        assert_eq!(wire_7.eval(), WireState::Z);
+        assert_eq!(wire_7.eval(), [WireState::Z]);
     }
 }
