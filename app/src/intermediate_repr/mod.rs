bfe6641987562285764fcc5be86ba0126338f304
mertwole <mertwole@gmail.com>
2026-02-09 17:54:05 +0000

    Introduce IdRegistry

--- app/src/intermediate_repr/mod.rs
+++ app/src/intermediate_repr/mod.rs
@@ -1,8 +1,38 @@
+use std::sync::{Arc, Mutex, OnceLock};
+
 pub mod flip_flop;
 pub mod gates;
 
+static ID_REGISTRY: OnceLock<IdRegistry> = OnceLock::new();
+
+pub struct IdRegistry {
+    last_id: Arc<Mutex<usize>>,
+}
+
+impl IdRegistry {
+    fn new() -> Self {
+        Self {
+            last_id: Arc::default(),
+        }
+    }
+}
+
 pub struct BusId {
     id: usize,
+}
+
+impl BusId {
+    pub fn new() -> Self {
+        let mut id = ID_REGISTRY
+            .get_or_init(IdRegistry::new)
+            .last_id
+            .lock()
+            .expect("Concurrency is not expected");
+        let new_id = *id;
+        *id += 1;
+
+        Self { id: new_id }
+    }
 }
 
 pub struct Module {
