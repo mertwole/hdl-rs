807013e18fe4dfee4fe2949dc8d3f0379018b68b
mertwole <mertwole@gmail.com>
2026-02-19 03:15:48 +0000

    Rearrange

--- app/src/intermediate_repr/mod.rs
+++ app/src/intermediate_repr/mod.rs
@@ -1,15 +1,39 @@
-use std::{
-    collections::{HashMap, hash_map::Entry},
-    fmt::{Binary, Display, Formatter},
-    hash::Hash,
-    process::Output,
-    sync::{Arc, Mutex, OnceLock},
-};
+use std::collections::{HashMap, hash_map::Entry};
 
-use crate::{
-    api::prelude::LogicalWireState,
-    verilog::{self, VerilogModule},
-};
+use crate::{api::prelude::LogicalWireState, verilog::VerilogModule};
+
+mod bus_id;
+pub use bus_id::*;
+
+pub struct IntermediateReprBuilder {
+    nodes: HashMap<BusId, Gate>,
+}
+
+// TODO: Add method `finalize` which will return `IntermediateRepr`.
+impl IntermediateReprBuilder {
+    pub fn new() -> Self {
+        Self {
+            nodes: HashMap::new(),
+        }
+    }
+
+    pub fn push_element(&mut self, element: Gate) {
+        if let Entry::Vacant(entry) = self.nodes.entry(element.get_id()) {
+            entry.insert(element);
+        }
+    }
+
+    // TODO: Move this fn to `VerilogModule::from_intermediate_repr`.
+    pub fn to_verilog(&self) -> VerilogModule {
+        let mut module = VerilogModule::new();
+
+        for node in self.nodes.values() {
+            node.to_verilog(&mut module);
+        }
+
+        module
+    }
+}
 
 pub enum Gate {
     Input(InputBus),
@@ -21,7 +45,13 @@ pub enum Gate {
 
 impl Gate {
     fn get_id(&self) -> BusId {
-        todo!()
+        match self {
+            Self::Input(input) => input.id,
+            Self::Const(const_bus) => const_bus.id,
+            Self::Unary(unary) => unary.output,
+            Self::Binary(binary) => binary.output,
+            Self::FlipFlop(flip_flop) => flip_flop.output,
+        }
     }
 
     fn to_verilog(&self, module: &mut VerilogModule) {
@@ -75,89 +105,4 @@ pub struct FlipFlop {
     pub clock: BusId,
 
     pub output: BusId,
-}
-
-static ID_REGISTRY: OnceLock<IdRegistry> = OnceLock::new();
-
-pub struct IdRegistry {
-    last_id: Arc<Mutex<usize>>,
-}
-
-impl IdRegistry {
-    fn new() -> Self {
-        Self {
-            last_id: Arc::default(),
-        }
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct BusId {
-    id: usize,
-    width: usize,
-}
-
-impl BusId {
-    pub fn new_unique(width: usize) -> Self {
-        let mut id = ID_REGISTRY
-            .get_or_init(IdRegistry::new)
-            .last_id
-            .lock()
-            .expect("Concurrency is not expected");
-        let new_id = *id;
-        *id += 1;
-
-        Self { id: new_id, width }
-    }
-
-    pub fn width(self) -> usize {
-        self.width
-    }
-
-    #[cfg(test)]
-    pub fn mock() -> Self {
-        Self { id: 0, width: 0 }
-    }
-}
-
-// TODO: Remove it. These names shouldn't appear on schematic and in verilog code.
-impl Display for BusId {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "bus_{}", self.id)
-    }
-}
-
-pub struct Module {
-    inputs: Vec<BusId>,
-    outputs: Vec<BusId>,
-}
-
-pub struct IntermediateReprBuilder {
-    nodes: HashMap<BusId, Gate>,
-}
-
-// TODO: Add method `finalize` which will return `IntermediateRepr`.
-impl IntermediateReprBuilder {
-    pub fn new() -> Self {
-        Self {
-            nodes: HashMap::new(),
-        }
-    }
-
-    pub fn push_element(&mut self, element: Gate) {
-        if let Entry::Vacant(entry) = self.nodes.entry(element.get_id()) {
-            entry.insert(element);
-        }
-    }
-
-    // TODO: Move this fn to `VerilogModule::from_intermediate_repr`.
-    pub fn to_verilog(&self) -> VerilogModule {
-        let mut module = VerilogModule::new();
-
-        for node in self.nodes.values() {
-            node.to_verilog(&mut module);
-        }
-
-        module
-    }
 }
