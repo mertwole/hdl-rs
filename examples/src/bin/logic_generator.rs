c3ed69a30f9093f3db6b77b0149c21fdf2830325
mertwole <mertwole@gmail.com>
2026-03-16 20:26:28 +0000

    Draft I

--- examples/src/bin/logic_generator.rs
+++ examples/src/bin/logic_generator.rs
@@ -1,7 +1,30 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 
+use std::marker::PhantomData;
+
 use hdl_rs::api::prelude::*;
+
+trait StageImpl<
+    const Input: usize,
+    const Output: usize,
+    const StageOutput: usize,
+    const Intermediate: usize,
+>
+{
+    fn stage(
+        input: impl Bus<Input>,
+        intermediate: impl Bus<Intermediate>,
+    ) -> (impl Bus<Intermediate>, impl Bus<StageOutput>);
+}
+
+struct Adder {}
+
+impl StageImpl<1, 1, 1, 1> for Adder {
+    fn stage(input: impl Bus<1>, intermediate: impl Bus<1>) -> (impl Bus<1>, impl Bus<1>) {
+        (input, intermediate)
+    }
+}
 
 fn main() {}
 
