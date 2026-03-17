1ec6afeef16615a34a306dfa659c11679a6ca084
mertwole <mertwole@gmail.com>
2026-03-17 09:40:11 +0000

    Draft III

--- examples/src/bin/logic_generator.rs
+++ examples/src/bin/logic_generator.rs
@@ -3,49 +3,24 @@
 
 use hdl_rs::api::prelude::*;
 
-struct StageOutput<const WO: usize, O: Bus<WO>, const WI: usize, I: Bus<WI>> {
-    pub output: O,
-    pub intermediate: I,
-}
-
-trait Generator<
-    Input,
-    const OUTPUT_WIDTH: usize,
-    const STAGE_OUTPUT_WIDTH: usize,
-    const INTERMEDIATE_WIDTH: usize,
->
-{
-    fn stage(
-        input: Input,
-        intermediate: impl Bus<INTERMEDIATE_WIDTH>,
-    ) -> StageOutput<
-        STAGE_OUTPUT_WIDTH,
-        impl Bus<STAGE_OUTPUT_WIDTH>,
-        INTERMEDIATE_WIDTH,
-        impl Bus<INTERMEDIATE_WIDTH>,
-    >;
-}
-
-struct Test {}
-
-struct TestInput<const W: usize, A: Bus<W>, B: Bus<W>> {
-    a: A,
-    b: B,
-}
-
-impl<const W: usize, A: Bus<W>, B: Bus<W>> Generator<TestInput<W, A, B>, W, 1, 1> for Test {
-    fn stage(
-        input: TestInput<W, A, B>,
-        intermediate: impl Bus<1>,
-    ) -> StageOutput<1, impl Bus<1>, 1, impl Bus<1>> {
-        StageOutput {
-            output: intermediate,
-            intermediate,
-        }
-    }
-}
-
 fn main() {}
+
+fn adder(a: impl Bus<8>, b: impl Bus<8>) -> impl Bus<8> {
+    const_for_loop!(
+        for I in 0..8
+        let carry = ConstBus::new([LogicalWireState::Zero]);
+        // In future: reduction op.
+    {
+        let ai = a.wire_at::<I>();
+        let bi = b.wire_at::<I>();
+
+        let carry = ai.and(bi).and(carry);
+
+        let out = carry.and(ai);
+
+        out
+    })
+}
 
 #[logic_generator(for I in 0..8)]
 fn _adder_stage<const I: usize>(
